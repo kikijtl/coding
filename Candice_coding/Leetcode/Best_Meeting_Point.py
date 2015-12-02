ec1682f01599af8261ee3a44eb8931671d098dfc
kiki <kiki@kiki-PC>
2015-12-02 15:27:18 -0800

    296

---
+++ Candice_coding/Leetcode/Best_Meeting_Point.py
@@ -0,0 +1,44 @@
+# A group of two or more people wants to meet and minimize the total travel distance. You are given a 2D grid of values 0 or 1, where each 1 marks the home of someone in the group. The distance is calculated using Manhattan Distance, where distance(p1, p2) = |p2.x - p1.x| + |p2.y - p1.y|.
+# 
+# For example, given three people living at (0,0), (0,4), and (2,2):
+# 
+# 1 - 0 - 0 - 0 - 1
+# |   |   |   |   |
+# 0 - 0 - 0 - 0 - 0
+# |   |   |   |   |
+# 0 - 0 - 1 - 0 - 0
+# The point (0,2) is an ideal meeting point, as the total travel distance of 2+2+2=6 is minimal. So return 6.
+# 
+# Hint:
+# 
+# Try to solve it in one dimension first. How can this solution apply to the two dimension case?
+
+
+class Solution(object):
+    def minTotalDistance(self, grid):
+        """
+        :type grid: List[List[int]]
+        :rtype: int
+        """
+        row = []
+        col = []
+        m = len(grid)
+        n = len(grid[0])
+        for i in xrange(m):
+            for j in xrange(n):
+                if grid[i][j]:
+                    row.append(i)
+                    col.append(j)
+        row.sort()
+        col.sort()
+        if len(row) % 2:
+            row_mid = row[len(row)/2]
+            col_mid = col[len(col)/2]
+        else:
+            row_mid = (row[len(row)/2-1] + row[len(row)/2]) / 2
+            col_mid = (col[len(col)/2-1] + col[len(col)/2]) / 2
+        d = 0
+        for k in xrange(len(row)):
+            d += abs(row[k]-row_mid) + abs(col[k]-col_mid)
+        return d
+        
