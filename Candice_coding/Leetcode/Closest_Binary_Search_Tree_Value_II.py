c41ee1e8e09d00f31242a303f5d461bb12804add
kikijtl <bestlovehot@gmail.com>
2016-02-14 12:17:46 -0800

    Update Closest_Binary_Search_Tree_Value_II.py
    272

--- Candice_coding/Leetcode/Closest_Binary_Search_Tree_Value_II.py
+++ Candice_coding/Leetcode/Closest_Binary_Search_Tree_Value_II.py
@@ -2,7 +2,7 @@
 # 
 # Note:
 # Given target value is a floating point.
-# You may assume k is always valid, that is: k ¡Ü total nodes.
+# You may assume k is always valid, that is: k <= total nodes.
 # You are guaranteed to have only one unique set of k values in the BST that are closest to the target.
 # Follow up:
 # Assume that the BST is balanced, could you solve it in less than O(n) runtime (where n = total nodes)?
